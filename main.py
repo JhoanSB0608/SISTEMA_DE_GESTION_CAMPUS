a69769749cd4126098c309fef2d67f6194925b06
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com>
2024-02-02 10:31:35 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import modulos.camper as camper
+
+camper.menu()
+
+# data = ["Hola","J", "Como Esra", "Bien Rey"]
+# for i,val in enumerate(sorted(data)):
+#     data[i] = val
+# print(f"{i+1}. {val}")
+# data = "".join([f"{i+1}. {val}\n" for i,val in enumerate(sorted(["Hola","J", "Como Esra", "Bien Rey"]))])
+# opc = int(input(data))
+# print(data[opc-1])
+
+# hola =[f"Data {i}" for i in [1,2,3,4,5,6]]
+# print(hola)
